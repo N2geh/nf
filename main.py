68288a01815038707a5d19ae6dcdfeea89589116
NAGEH <64553859+N2geh@users.noreply.github.com>
2022-10-08 19:50:47 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ API_HASH = "61653dd713fdb95f10de8f4bf6407b30"
 BOT_TOKEN = "5728995132:AAG_LI2dmHUvPtX1SyATgEfp9-mx32oP8iQ"
 
 NAGEHX = Client(
-  name="PyrogramBot",
-  api_id=API_ID,
-  api_hash=API_HASH,
-  bot_token=BOT_TOKEN
+    name="PyrogramBot",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN
 )
 
 print("Bot Started")
